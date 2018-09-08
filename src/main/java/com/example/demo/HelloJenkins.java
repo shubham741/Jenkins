d0ea93004e95ac0cc73a6e523a1b4d403897cc6f
Shubham Singh <30573769+shubham741@users.noreply.github.com>
2018-09-08 23:50:55 +0530

    Update HelloJenkins.java

--- src/main/java/com/example/demo/HelloJenkins.java
+++ src/main/java/com/example/demo/HelloJenkins.java
@@ -1,1 +1,13 @@
+package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloJenkins {
+
+  @GetMapping("/hello")
+	public String hello() {
+		return "Hello From Jenkins and Docker";
+	}
+}
